4709496799cd681315e016fff287faba1b5cdec0
chikechris <cchike2017@gmail.com>
2020-05-10 04:12:08 -0400

    db connected

--- server/server.js
+++ server/server.js
@@ -1,27 +1,37 @@
-const express = require('express')
+const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+//app
+const app = express();
 
-//app 
-const app = express() 
+// db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('db connected'))
+  .catch((err) => console.log(err));
 
-//middlewares 
-app.use(cors())
-app.use(morgan('dev')) 
-app.use(bodyParser.json()) 
+//middlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
-//route 
+//route
 app.get('*', (req, res) => {
   res.json({
-    response: 'this is a test'
-  })  
-})  
+    response: 'this is a very big test',
+  });
+});
 
-//port 
+//port
 
-const port = process.env.PORT || 7000 
-app.listen(port, () => console.log(`Server is Up on Port ${port}`))
+const port = process.env.PORT || 7000;
+app.listen(port, () => console.log(`Server is Up on Port ${port}`));
